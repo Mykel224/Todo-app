8b02bd8353a3a0bee432271cfb5db5a560ed98a4
unknown <odutolacovenantadewale@gmail.com>
2023-12-13 16:13:13 +0100

    Initial commit

--- src/screen/TodoScreen.js
+++ src/screen/TodoScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, FlatList } from "react-native";
+import { AntDesign } from '@expo/vector-icons'; // Import AntDesign icons
 
 function TodoScreen() {
   const [todo, setTodo] = useState("");
@@ -18,6 +19,27 @@ function TodoScreen() {
     setTasks(updatedTasks);
   };
 
+  const renderItem = ({ item, index }) => (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 6,
+        marginBottom: 10,
+        justifyContent: 'space-between',
+      }}
+    >
+      <Text>{index + 1}. {item}</Text>
+      <TouchableOpacity onPress={() => handleDeleteTodo(index)}>
+        <AntDesign name="delete" size={20} color="red" />
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={{ marginHorizontal: 16, marginVertical: 40 }}>
       <TextInput
@@ -27,21 +49,19 @@ function TodoScreen() {
           borderRadius: 6,
           paddingVertical: 8,
           paddingHorizontal: 16,
+          marginBottom: 20,
         }}
-        placeholder="Add a task"
+        placeholder="Write a text"
         value={todo}
         onChangeText={(text) => setTodo(text)}
       />
       <TouchableOpacity
         style={{
-          backgroundColor: "black",
+          backgroundColor: "blue",
           paddingVertical: 12,
-          marginVertical: 20,
           borderRadius: 6,
-          shadowColor: "#000",
-          shadowOffset: { width: 0, height: 2 },
-          shadowOpacity: 0.5,
-          shadowRadius: 3,
+          alignItems: 'center',
+          marginBottom: 20,
         }}
         onPress={handleAddTodo}
       >
@@ -49,36 +69,15 @@ function TodoScreen() {
           style={{
             color: "white",
             fontWeight: "bold",
-            fontSize: 20,
-            textAlign: "center",
+            fontSize: 18,
           }}
         >
-          Add
+          Add Task
         </Text>
       </TouchableOpacity>
       <FlatList
         data={tasks}
-        renderItem={({ item, index }) => (
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              padding: 10,
-              borderWidth: 1,
-              borderColor: '#ccc',
-              borderRadius: 6,
-              marginBottom: 10,
-            }}
-          >
-            <Text>{item}</Text>
-            <TouchableOpacity
-              onPress={() => handleDeleteTodo(index)}
-            >
-              <Text style={{ color: 'red', marginLeft: 10 }}>Delete</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+        renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
       />
     </View>
